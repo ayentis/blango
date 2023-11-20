7e0052baa5ee2705e60f95bb088b410e92390781
Oleksiy YENTIS <oleksiy.yentis@capgemini.com>
2023-11-20 08:42:27 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,7 @@ from blog.models import Tag, Post, Comment
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ("slug", "published_at")
 
 admin.site.register(Tag)
 admin.site.register(Comment)
 admin.site.register(Post, PostAdmin)
-
